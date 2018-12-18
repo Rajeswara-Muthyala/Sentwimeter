48fbee72bab43f4ee7cb3b6045972ed21db656b0
Rajeswara <raja.muthyala@gmail.com>
2018-12-18 12:46:40 +0530

    Multi Request aggregation etc

--- Sentwimeter.py
+++ Sentwimeter.py
@@ -5,26 +5,33 @@ import argparse
 ####main####
 
 parser = argparse.ArgumentParser()
+
+### Start of arguments ###
 parser.add_argument("keyword", help = "keyword to search for")
-parser.add_argument("count", help = "Number of tweets to consider(limit of 100)")
 parser.add_argument("since_date", help = "The date from which tweets should be analyzed")
+parser.add_argument("-count", "--tweet_count", type=int, help = "Number of tweets to consider - *Warning* This may trigger multiple search requests. Each twitter search requests has a limitation 100 tweets. Default will be best effort", default=1000)
 parser.add_argument("-st", "--search_type", help = "recent(default)/popular/mixed", default="recent")
 parser.add_argument("-lang", "--language", help = "language tweets", default="en")
+### End of arguments ###
+
 args = parser.parse_args()
 
 tClient = TwitterClient()
-search_results = tClient.search_until(k=args.keyword, tc=args.count, st=args.search_type, ud=args.since_date, lan = args.language)
+if args.tweet_count <= 100:
+    search_results = tClient.search_until(k=args.keyword, tc=args.tweet_count, st=args.search_type, ud=args.since_date, lan = args.language)
+    opin_dict = tClient.opinion_mining(search_results['statuses'])
+    '''
+    te_search_results = twitter.search(q=keyword, count=tweet_count, result_type='mixed', since_id=since, lang="te")
 
-'''
-te_search_results = twitter.search(q=keyword, count=tweet_count, result_type='mixed', since_id=since, lang="te")
+    trans = Translator()
+    for result in te_search_results['statuses']:
+        print result['text']
+        trans.translate(result['text'])
+    '''
+else:
+    multi_search_results = tClient.multi_search_until(k=args.keyword, ud=args.since_date, lan = args.language)
+    opin_dict = tClient.opinion_mining_multi(multi_search_results)
 
-trans = Translator()
-for result in te_search_results['statuses']:
-    print result['text']
-    trans.translate(result['text'])
-'''
-
-opin_dict = tClient.opinion_mining(search_results)
 
 while True:
     print "Print 1)Positive 2)Negative 3)Netral 4)Exit \n "
--- twitterclient.py
+++ twitterclient.py
@@ -50,9 +50,81 @@ class TwitterClient():
         print "since_id is ...", single_tweet['statuses'][0]['id'], "created at", single_tweet['statuses'][0]['created_at']
         since = single_tweet['statuses'][0]['id']
 
-        pp = pprint.PrettyPrinter(indent=1)
         search_results = self.twitter.search(q=k, count=tc, result_type=st, since_id=since, lang = lan, tweet_mode='extended')
         return search_results
+
+    def multi_search_until(self, k, ud, lan = 'en'):
+        single_tweet = self.twitter.search(q=k, count=1, result_type="recent", until=ud)
+        if len(single_tweet['statuses']) != 1:
+            print "More than a single tweet for fetching since_id, exiting...", len(single_tweet['statuses'])
+            exit(0)
+
+        print "since_id is ...", single_tweet['statuses'][0]['id'], "created at", single_tweet['statuses'][0]['created_at']
+        since = single_tweet['statuses'][0]['id']
+
+        results = []
+        result = self.twitter.search(q=k, count=100, result_type="recent", lang = lan, tweet_mode='extended')
+        results.append(result)
+        iter = 0
+        
+        while iter <= 10:
+            # Get max_id of results
+            print result['search_metadata']
+            try:
+                next_max_id = result['search_metadata']['next_results'].split('max_id=')[1].split('&')[0]
+            except:
+                print "No more next pages"
+                break
+
+            if next_max_id <= since:
+                break
+
+            # Use max_id in next request 
+            result = self.twitter.search(q=k, count=100, max_id=next_max_id, lang = lan, tweet_mode='extended')
+            results.append(result)
+            iter += 1
+
+        print "Am i here"
+        return results
+
+    def opinion_mining_multi(self, multi_search_results):
+        num_tweets = 0
+        positive = 0
+        negative = 0
+        neutral = 0
+
+        opinion_dict = {"positive":{}, \
+                        "negative":{}, \
+                        "neutral":{}}
+
+        for i in multi_search_results:
+                            
+            num_tweets += len(i['statuses'])
+            for tweet in i['statuses']:
+                #print("Created at ", tweet['created_at'], tweet['text'])
+                sentimeter = self.get_tweet_sentiment(tweet['full_text'])
+                if sentimeter == "positive":
+                    opinion_dict["positive"][tweet['id']] = tweet
+                    positive+=1
+                elif sentimeter == "negative":
+                    opinion_dict["negative"][tweet['id']] = tweet
+                    negative+=1    
+                else:
+                    opinion_dict["neutral"][tweet['id']] = tweet
+                    neutral+=1
+
+        positive_percetage = (float(positive) / num_tweets) * 100
+        negative_percetage = (float(negative) / num_tweets) * 100
+        neutral_percetage = (float(neutral) / num_tweets) * 100
+
+        #print "keyword ", keyword, "is trending in twitter as below:"
+        print "Positive :) ", positive_percetage
+        print "Negative :( ", negative_percetage
+        print "neutral  :| ", neutral_percetage
+
+        print "\n Assessed on ", num_tweets, " tweets"
+
+        return opinion_dict
 
     def opinion_mining(self, search_results):
         #Dictionary 
@@ -60,11 +132,11 @@ class TwitterClient():
                         "negative":{}, \
                         "neutral":{}}
                         
-        num_tweets = len(search_results['statuses'])
+        num_tweets = len(search_results)
         positive = 0
         negative = 0
         neutral = 0
-        for tweet in search_results['statuses']:
+        for tweet in search_results:
             #print("Created at ", tweet['created_at'], tweet['text'])
             sentimeter = self.get_tweet_sentiment(tweet['full_text'])
             if sentimeter == "positive":
